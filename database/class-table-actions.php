b38c36718a13911a9d74979b9b8e1dfd32966203
valentingenev <valentin_genev@abv.bg>
2020-04-19 16:44:43 +0300

    - index created for the messages table; - deprecated files removed;

--- database/class-table-actions.php
+++ database/class-table-actions.php
@@ -12,16 +12,16 @@ class Table_Actions {
 
 	function create_messages_table() {
 		$messages_table = '
-			CREATE TABLE IF NOT EXISTS messages (
+			CREATE TABLE IF NOT EXISTS Messages (
 				id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-				poster TEXT,
+				poster VARCHAR(256),
 				post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 				message TEXT
 			);
-			CREATE INDEX poster_index ON messages (poster);
+			CREATE INDEX poster_index ON Messages (poster(256));
 		';
 
-		if ($this->db_connection->query($messages_table) === true) {
+		if ($this->db_connection->multi_query($messages_table)) {
 			return 'The table is ready.';
 		}
 		else {
