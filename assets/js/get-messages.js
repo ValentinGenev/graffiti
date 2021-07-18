943b7ba7ec984846c489d0817280b4bdc83a2abd
valentingenev <valentin_genev@abv.bg>
2021-07-18 17:40:29 +0300

    Promises refactor: [=]  post-messages.js completely refactored to use async; [=] post responses sligtly altered; [=] vertical alignment of var declarations removed; [=] functions.js renamed to utilities.js;

--- assets/js/get-messages.js
+++ assets/js/get-messages.js
@@ -1,28 +1,28 @@
 window.addEventListener("load", () => {
-	const SCREEN_HEIGHT			= getScreenHeight()
-	const BODY							= document.querySelector('body')
-	const MESSAGE_CONTAINER	= document.querySelector('#messages_container')
+	const SCREEN_HEIGHT = getScreenHeight()
+	const BODY = document.querySelector('body')
+	const MESSAGE_CONTAINER = document.querySelector('#messages_container')
 
 
 	// Initial GET call
 	customGetFetch({ count: 10 }, showMessages)
-	.then(() => {
-		fillScreenWithMessages(BODY, SCREEN_HEIGHT)
-	})
+		.then(() => {
+			fillScreenWithMessages(BODY, SCREEN_HEIGHT)
+		})
 
 
 	// Load more messages when near the bottom of the list
-	const ENDLESS_SCROLL		= throttle(() => {
+	const ENDLESS_SCROLL = throttle(() => {
 		if (window.pageYOffset + SCREEN_HEIGHT > BODY.offsetHeight - 100) {
 			window.removeEventListener('scroll', ENDLESS_SCROLL)
 
 			customGetFetch({ count: 10, oldest_loaded: MESSAGE_CONTAINER.dataset.oldestLoadedMessage }, showMessages)
-			.then(() => {
-				window.addEventListener('scroll', ENDLESS_SCROLL)
-			})
-			.catch(({ status, responseText }) => {
-				console.log(status === 404 ? 'All messages were loaded.' : responseText)
-			})
+				.then(() => {
+					window.addEventListener('scroll', ENDLESS_SCROLL)
+				})
+				.catch(({ status, responseText }) => {
+					console.log(status === 404 ? 'All messages were loaded.' : responseText)
+				})
 		}
 	}, 50)
 
@@ -33,7 +33,7 @@ window.addEventListener("load", () => {
 	window.addEventListener('focus', () => {
 		let lastPost = document.querySelector('.message')
 
-		customGetFetch({count: 10, newest_loaded: lastPost.id }, showNewestMessages)
+		customGetFetch({ count: 10, newest_loaded: lastPost.id }, showNewestMessages)
 	})
 })
 
@@ -45,18 +45,18 @@ window.addEventListener("load", () => {
  * @param {Number} screenHeight
  */
 function fillScreenWithMessages(body, screenHeight) {
-	let oldestLoadedMessage	= document.querySelector('#messages_container').dataset.oldestLoadedMessage
-	let scrollOffset				= screenHeight + 200
+	let oldestLoadedMessage = document.querySelector('#messages_container').dataset.oldestLoadedMessage
+	let scrollOffset = screenHeight + 200
 
 	customGetFetch({ count: 10, oldest_loaded: oldestLoadedMessage }, showMessages)
-	.then(({ status }) => {
-		if (body.offsetHeight < scrollOffset && status < 400) {
-			fillScreenWithMessages(body, screenHeight)
-		}
-	})
-	.catch(({ responseText }) => {
-		console.log(responseText)
-	})
+		.then(({ status }) => {
+			if (body.offsetHeight < scrollOffset && status < 400) {
+				fillScreenWithMessages(body, screenHeight)
+			}
+		})
+		.catch(({ responseText }) => {
+			console.log(responseText)
+		})
 }
 
 
@@ -66,8 +66,8 @@ function fillScreenWithMessages(body, screenHeight) {
  * @param {JSON} messages
  */
 function showMessages(messages) {
-	const MESSAGE_CONTAINER	= document.querySelector('#messages_container')
-	const MESSAGES					= JSON.parse(messages)
+	const MESSAGE_CONTAINER = document.querySelector('#messages_container')
+	const MESSAGES = JSON.parse(messages)
 
 	MESSAGE_CONTAINER.dataset.oldestLoadedMessage = MESSAGES[MESSAGES.length - 1].id
 	MESSAGES.forEach(entry => MESSAGE_CONTAINER.appendChild(renderMessageEntry(entry, 'old')))
@@ -80,9 +80,9 @@ function showMessages(messages) {
  * @param {JSON} messages
  */
 function showNewerMessages(messages) {
-	const MESSAGE_CONTAINER				= document.querySelector('#messages_container')
-	const LATEST_ARCHIVED_MESSAGE	= document.querySelector('.message.old')
-	const MESSAGES								= JSON.parse(messages)
+	const MESSAGE_CONTAINER = document.querySelector('#messages_container')
+	const LATEST_ARCHIVED_MESSAGE = document.querySelector('.message.old')
+	const MESSAGES = JSON.parse(messages)
 
 	MESSAGES.forEach(entry => MESSAGE_CONTAINER.insertBefore(renderMessageEntry(entry, 'missed'), LATEST_ARCHIVED_MESSAGE))
 }
@@ -94,9 +94,9 @@ function showNewerMessages(messages) {
  * @param {JSON} messages
  */
 function showNewestMessages(messages) {
-	const MESSAGE_CONTAINER				= document.querySelector('#messages_container')
-	const LATEST_ARCHIVED_MESSAGE	= document.querySelector('.message')
-	const MESSAGES								= JSON.parse(messages)
+	const MESSAGE_CONTAINER = document.querySelector('#messages_container')
+	const LATEST_ARCHIVED_MESSAGE = document.querySelector('.message')
+	const MESSAGES = JSON.parse(messages)
 
 	MESSAGES.forEach(entry => MESSAGE_CONTAINER.insertBefore(renderMessageEntry(entry, 'missed old'), LATEST_ARCHIVED_MESSAGE))
 }
@@ -116,20 +116,20 @@ function customGetFetch(data, callback = (responseText) => console.log(responseT
 
 	return new Promise((resolve, reject) => {
 		fetch(urlWithParams, { method: 'GET' })
-		.then(response => {
-			let responseStatus = response.status
+			.then(response => {
+				let responseStatus = response.status
 
-			return response.text().then(response => { return { status: responseStatus, responseText: response } })
+				return response.text().then(response => { return { status: responseStatus, responseText: response } })
 
-		})
-		.then(data => {
-			if (data.status === 200) {
-				callback(data.responseText)
-				resolve({ status: data.status, responseText: 'Messages rendered.' })
-			}
-			else {
-				reject({ status: data.status, responseText: data.responseText })
-			}
-		})
+			})
+			.then(data => {
+				if (data.status === 200) {
+					callback(data.responseText)
+					resolve({ status: data.status, responseText: 'Messages rendered.' })
+				}
+				else {
+					reject({ status: data.status, responseText: data.responseText })
+				}
+			})
 	})
 }
